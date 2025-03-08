da561722034d908f58b5157d65c9127c89cb32e3
nhp <ricardoryn1317@gmail.com>
2025-03-08 13:37:06 +0800

    add more plugins

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
 require("full-border"):setup()
+require("git"):setup()
+-- show symlink in status bar
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+-- cross instance yank
+require("session"):setup {
+	sync_yanked = true,
+}
