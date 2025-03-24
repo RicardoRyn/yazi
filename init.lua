1f6432e4eb96cd9c19cb70ae597daef538b5a164
nhp <ricardoryn1317@gmail.com>
2025-03-24 13:59:55 +0800

    update

--- init.lua
+++ init.lua
@@ -2,14 +2,16 @@ require("full-border"):setup()
 require("git"):setup()
 -- show symlink in status bar
 Status:children_add(function(self)
-	local h = self._current.hovered
-	if h and h.link_to then
-		return " -> " .. tostring(h.link_to)
-	else
-		return ""
-	end
+  local h = self._current.hovered
+  if h and h.link_to then
+    return " -> " .. tostring(h.link_to)
+  else
+    return ""
+  end
 end, 3300, Status.LEFT)
 -- cross instance yank
-require("session"):setup {
-	sync_yanked = true,
-}
+require("session"):setup({
+  sync_yanked = true,
+})
+-- cloese preview panel when start yazi
+require("toggle-pane"):entry("min-preview")
